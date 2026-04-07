710047c15e37279505ccba7ec4c6163f3c33ea94
Simon Huang <105900389+ahhshiba@users.noreply.github.com>
2026-04-08 01:14:16 +0800

    Update independent_v8.py

--- independent_v8.py
+++ independent_v8.py
@@ -75,7 +75,7 @@ def letterbox_image(img, expected_size):
 
 def run_single_inference(model, frame):
     # 每個 frame 都由自己的 YOLO 模型推論
-    results = model(frame, conf=0.30, verbose=False, device='0')
+    results = model(frame, conf=0.30, verbose=False, device='0') # 根據系統給的顯卡編號修改device編號 (nvidia-smi)
     return results[0].plot()
 
 
